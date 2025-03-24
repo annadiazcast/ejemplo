701a5d692f3e61cac9b26c4871729be5bd0d9e85
annab <112609@alumnouninter.mx>
2025-03-24 15:50:50 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
